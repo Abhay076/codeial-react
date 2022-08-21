571f087d8578c839b5834d5b90dc78e15ee112db
Abhay 076 <yadavabhaycs@gmail.com>
2022-08-21 13:12:54 +0530

    Added Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import propTypes from 'prop-types';
 import { fetchPosts } from '../actions/posts';
-import {PostList} from './';
+import { PostList } from './';
 
 class App extends React.Component {
   componentDidMount() {
@@ -13,7 +13,59 @@ class App extends React.Component {
     const { posts } = this.props;
     return (
       <div>
-       <PostList posts={posts}/>
+        <nav className="nav">
+          <div className="left-div">
+            <img
+              src="https://ninjasfiles.s3.amazonaws.com/0000000000003454.png"
+              alt="logo"
+            />
+          </div>
+          <div className="search-container">
+            <img
+              className="search-icon"
+              src="https://cdn-icons-png.flaticon.com/512/54/54481.png"
+              alt="search-icon"
+            />
+            <input placeholder="Search" />
+
+            <div className="search-results">
+              <ul>
+                <li className="search-results-row">
+                  <img
+                    src="https://cdn-icons.flaticon.com/png/512/2202/premium/2202112.png?token=exp=1661068334~hmac=df652c14502263acfe6f24e1a308826a"
+                    alt="user-dp"
+                  />
+                  <span>John Doe</span>
+                </li>
+                <li className="search-results-row">
+                  <img
+                    src="https://cdn-icons.flaticon.com/png/512/1144/premium/1144709.png?token=exp=1661068243~hmac=91fa670a2a8ee3afaa2c220a1571c637"
+                    alt="user-dp"
+                  />
+                  <span>John Doe</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="right-nav">
+            <div className="user">
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/3237/3237472.png"
+                alt="user-dp"
+                id="user-dp"
+              />
+              <span>John Doe</span>
+            </div>
+            <div className="nav-links">
+              <ul>
+                <li>Log in</li>
+                <li>Log out</li>
+                <li>Register</li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <PostList posts={posts} />
       </div>
     );
   }
