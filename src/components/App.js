bc26b3fba15d5954c9600432dc5a59dd3c3cff87
Abhay 076 <yadavabhaycs@gmail.com>
2022-08-21 21:56:32 +0530

    Add react Router 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { BrowserRouter as Router, Routes, Link, Route } from 'react-router-dom';
 import propTypes from 'prop-types';
 import { fetchPosts } from '../actions/posts';
-import { PostList } from './';
-
+import { PostList, Navbar } from './';
+const Login = () => <div>Login</div>;
+const Signup = () => <div>Signup</div>;
+const Home = () => <div>Home</div>;
 class App extends React.Component {
   componentDidMount() {
     this.props.dispatch(fetchPosts());
@@ -11,62 +14,31 @@ class App extends React.Component {
   render() {
     console.log('props', this.props);
     const { posts } = this.props;
+    
     return (
-      <div>
-        <nav className="nav">
-          <div className="left-div">
-            <img
-              src="https://ninjasfiles.s3.amazonaws.com/0000000000003454.png"
-              alt="logo"
-            />
-          </div>
-          <div className="search-container">
-            <img
-              className="search-icon"
-              src="https://cdn-icons-png.flaticon.com/512/54/54481.png"
-              alt="search-icon"
-            />
-            <input placeholder="Search" />
-
-            <div className="search-results">
-              <ul>
-                <li className="search-results-row">
-                  <img
-                    src="https://cdn-icons.flaticon.com/png/512/2202/premium/2202112.png?token=exp=1661068334~hmac=df652c14502263acfe6f24e1a308826a"
-                    alt="user-dp"
-                  />
-                  <span>John Doe</span>
-                </li>
-                <li className="search-results-row">
-                  <img
-                    src="https://cdn-icons.flaticon.com/png/512/1144/premium/1144709.png?token=exp=1661068243~hmac=91fa670a2a8ee3afaa2c220a1571c637"
-                    alt="user-dp"
-                  />
-                  <span>John Doe</span>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="right-nav">
-            <div className="user">
-              <img
-                src="https://cdn-icons-png.flaticon.com/512/3237/3237472.png"
-                alt="user-dp"
-                id="user-dp"
-              />
-              <span>John Doe</span>
-            </div>
-            <div className="nav-links">
-              <ul>
-                <li>Log in</li>
-                <li>Log out</li>
-                <li>Register</li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-        <PostList posts={posts} />
-      </div>
+      <Router>
+        <div>
+          <Navbar />
+          {/* <PostList posts={posts} /> */}
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </ul>
+          <Routes>
+            <Route exact path="/" element={<Home/>} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/signup" element={<Signup/>} />
+          </Routes>
+        </div>
+      </Router>
+      
     );
   }
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+function Navbar(props) {
+    return (
+        <nav className="nav">
+        <div className="left-div">
+          <img
+            src="https://ninjasfiles.s3.amazonaws.com/0000000000003454.png"
+            alt="logo"
+          />
+        </div>
+        <div className="search-container">
+          <img
+            className="search-icon"
+            src="https://cdn-icons-png.flaticon.com/512/54/54481.png"
+            alt="search-icon"
+          />
+          <input placeholder="Search" />
+
+          <div className="search-results">
+            <ul>
+              <li className="search-results-row">
+                <img
+                  src="https://cdn-icons.flaticon.com/png/512/2202/premium/2202112.png?token=exp=1661068334~hmac=df652c14502263acfe6f24e1a308826a"
+                  alt="user-dp"
+                />
+                <span>John Doe</span>
+              </li>
+              <li className="search-results-row">
+                <img
+                  src="https://cdn-icons.flaticon.com/png/512/1144/premium/1144709.png?token=exp=1661068243~hmac=91fa670a2a8ee3afaa2c220a1571c637"
+                  alt="user-dp"
+                />
+                <span>John Doe</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="right-nav">
+          <div className="user">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/3237/3237472.png"
+              alt="user-dp"
+              id="user-dp"
+            />
+            <span>John Doe</span>
+          </div>
+          <div className="nav-links">
+            <ul>
+              <li>Log in</li>
+              <li>Log out</li>
+              <li>Register</li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+}
+
+export default Navbar;
