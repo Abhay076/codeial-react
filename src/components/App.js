bc04ab9900cb14bb09f3431e70039b0500a88b79
Abhay 076 <yadavabhaycs@gmail.com>
2022-08-22 17:29:43 +0530

    Added Router-II

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { BrowserRouter as Router, Routes, Link, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
 import propTypes from 'prop-types';
 import { fetchPosts } from '../actions/posts';
-import { PostList, Navbar } from './';
+import { Home, Navbar, Page404 } from './';
 const Login = () => <div>Login</div>;
 const Signup = () => <div>Signup</div>;
-const Home = () => <div>Home</div>;
+
 class App extends React.Component {
   componentDidMount() {
     this.props.dispatch(fetchPosts());
@@ -14,31 +14,25 @@ class App extends React.Component {
   render() {
     console.log('props', this.props);
     const { posts } = this.props;
-    
+
     return (
       <Router>
         <div>
           <Navbar />
-          {/* <PostList posts={posts} /> */}
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/signup">Signup</Link>
-            </li>
-          </ul>
+          {/* <Home posts={posts} /> */}
+
           <Routes>
-            <Route exact path="/" element={<Home/>} />
-            <Route path="/login" element={<Login/>} />
-            <Route path="/signup" element={<Signup/>} />
+            <Route
+              exact
+              path="/"
+              element={<Home posts={posts}/>}
+            />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/signup" element={<Signup />} />
+            <Route path="*" element={<Page404/>} />
           </Routes>
         </div>
       </Router>
-      
     );
   }
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
-import PostList from "./PostList";
+import PostList from './PostList';
 // import App from "./App";
-import Navbar from "./Navbar";
-
-export {PostList,Navbar};
+import Navbar from './Navbar';
+import Home from './Home';
+import Page404 from './Page404';
+export { PostList, Navbar, Home, Page404 };
