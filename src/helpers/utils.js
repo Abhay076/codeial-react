138cca7e03952463c3b858234141df6735d56f7a
Abhay 076 <yadavabhaycs@gmail.com>
2022-08-22 20:18:05 +0530

    Logging user in -II

---
+++ src/helpers/utils.js
@@ -0,0 +1,11 @@
+export function getFormBody(params) {
+  let formBody = [];
+
+  for (let property in params) {
+    let encodedKey = encodeURIComponent(property);//'user name' => 'user%20name'
+    let encodedValue = encodeURIComponent(params[property]);// abhay 123 => abhay%2020123
+
+    formBody(encodedKey + '=' + encodedValue);
+  }
+   return formBody.join('&');// 'username=abhay&password=12341'
+}
