2f9f6140db0c7c8658f32f5df69109ae32e7ae25
Abhay 076 <yadavabhaycs@gmail.com>
2022-08-25 16:44:21 +0530

    Persisting the user-II

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import propTypes from 'prop-types';
 import { fetchPosts } from '../actions/posts';
 import { Home, Navbar, Page404, Login, Signup } from './';
 import jwtDecode from 'jwt-decode';
+import { authenticateUser } from '../actions/auth';
 
 class App extends React.Component {
   componentDidMount() {
     this.props.dispatch(fetchPosts());
     const token = localStorage.getItem('token');
-    if(token){
+    if (token) {
       const user = jwtDecode(token);
-      console.log('user',user);
+      console.log('user', user);
+      this.props.dispatch(
+        authenticateUser({
+          email: user.email,
+          _id: user._id,
+          name: user.name,
+        })
+      );
     }
   }
   render() {
@@ -26,14 +34,10 @@ class App extends React.Component {
           {/* <Home posts={posts} /> */}
 
           <Routes>
-            <Route
-              exact
-              path="/"
-              element={<Home posts={posts}/>}
-            />
+            <Route exact path="/" element={<Home posts={posts} />} />
             <Route exact path="/login" element={<Login />} />
             <Route exact path="/signup" element={<Signup />} />
-            <Route path="*" element={<Page404/>} />
+            <Route path="*" element={<Page404 />} />
           </Routes>
         </div>
       </Router>
