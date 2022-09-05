195d8014ab67821af0fbf5381a210229a5253b3a
Abhay 076 <yadavabhaycs@gmail.com>
2022-09-05 23:09:46 +0530

    Searching users: action and reducers

---
+++ src/actions/search.js
@@ -0,0 +1,30 @@
+import { FETCH_SEARCH_RESULTS_SUCCESS } from './actionTypes';
+import { getAuthTokenFromLocalStorage } from '../helpers/utils';
+import { APIUrls } from '../helpers/urls';
+export function searchUsers(searchText) {
+  return (dispatch) => {
+    const url = APIUrls.userSearch(searchText);
+    fetch(url, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: `Bearer ${getAuthTokenFromLocalStorage()}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('SEARCH DATA', data);
+        if (data.success) {
+          dispatch(searchResultsSuccess(data.data.users));
+        } else {
+          dispatch(searchResultsSuccess([]));
+        }
+      });
+  };
+}
+
+export function searchResultsSuccess(user) {
+  return {
+    tyep: FETCH_SEARCH_RESULTS_SUCCESS,
+    user,
+  };
+}
